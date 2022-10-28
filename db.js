5eb28c0c60da84574ca01b1c3df5a34576c9e668
hiredtocode <jasonhan@kakao.com>
2022-10-28 20:44:45 +0900

    New build

--- db.js
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from "firebase";
-import { firebaseConfig } from "config";
-
-const db = initializeApp(firebaseConfig);
-
-export default db;
--- routes/student-routes.js
+++
@@ -1,8 +0,0 @@
-import { router } from "express";
-import { addStudent } from "../controllers/studentController.js";
-
-const _router = router();
-
-_router.post("/student", addStudent);
-
-export const routes = _router;
